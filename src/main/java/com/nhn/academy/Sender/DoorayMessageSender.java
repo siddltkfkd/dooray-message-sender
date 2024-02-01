d274391f7a0d2f2dcc7e8cf0268a8862056382f3
dmswl <siddltkfkd@gmail.com>
2024-02-01 15:07:31 +0900

    DoorayHookSender, RestTemplate을 bean으로

--- src/main/java/com/nhn/academy/Sender/DoorayMessageSender.java
+++ src/main/java/com/nhn/academy/Sender/DoorayMessageSender.java
@@ -3,25 +3,20 @@ package com.nhn.academy.Sender;
 import com.nhn.academy.domain.User;
 import com.nhn.dooray.client.DoorayHook;
 import com.nhn.dooray.client.DoorayHookSender;
-import org.springframework.web.client.RestTemplate;
 
 public class DoorayMessageSender implements MessageSender{
-    private String url;
+    private DoorayHookSender doorayHookSender;
 
-    public DoorayMessageSender() {
+    public DoorayMessageSender(DoorayHookSender doorayHookSender) {
+        this.doorayHookSender = doorayHookSender;
     }
 
     @Override
     public boolean sendMessage(User user, String message) {
-        new DoorayHookSender(new RestTemplate(), url)
-                .send(DoorayHook.builder()
+        doorayHookSender.send(DoorayHook.builder()
                         .botName(user.getName())
                         .text(message)
                         .build());
         return true;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
     }
 }

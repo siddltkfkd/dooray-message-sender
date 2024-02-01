ecdc1ace6d9d96e7c80423fc415c981a0d20f51a
dmswl <siddltkfkd@gmail.com>
2024-02-01 19:22:32 +0900

    Test 작성

---
+++ src/test/java/com/nhn/academy/DoorayMessageSenderTest.java
@@ -0,0 +1,39 @@
+package com.nhn.academy;
+
+import com.nhn.academy.Sender.DoorayMessageSender;
+import com.nhn.academy.domain.User;
+import com.nhn.dooray.client.DoorayHookSender;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Matchers.any;
+
+public class DoorayMessageSenderTest {
+    @Mock
+    private DoorayHookSender doorayHookSender;
+    @InjectMocks
+    private DoorayMessageSender doorayMessageSender;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void verifySendMessage(){
+        doorayMessageSender.sendMessage(new User("테스트"), "테스트 메세지");
+        Mockito.verify(doorayHookSender).send(any());
+    }
+
+    @Test
+    void verifySendMessageCount(){
+        doorayMessageSender.sendMessage(new User("테스트"), "테스트 메세지");
+        doorayMessageSender.sendMessage(new User("테스트"), "테스트 메세지");
+        doorayMessageSender.sendMessage(new User("테스트"), "테스트 메세지");
+        Mockito.verify(doorayHookSender, Mockito.times(3)).send(any());
+    }
+}
